0edb717265be482f32916ef01902695a831ea94b
Faraz-mobin17 <faraz.mobin17@gmail.com>
2024-02-14 22:22:58 +0530

    ✅ added server config

---
+++ backend/config/serverConfig.js
@@ -0,0 +1,18 @@
+import fs from "fs";
+import dotenv from "dotenv";
+
+import { dotenvPath } from "../path";
+
+if (fs.existsSync(dotenvPath)) {
+  dotenv.config(dotenvPath);
+} else {
+  console.error("Error dot env file doesn't exits");
+  process.exit();
+}
+
+const envVars = {
+  PORT: process.env.PORT || 5000,
+  MONGODB_URI: process.env.MONGODB_URI || "mongodb://localhost:27017",
+};
+
+export { envVars };
