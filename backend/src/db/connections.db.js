6238f01393c903a4eab1a4067edf20a545281b0a
Faraz-mobin17 <faraz.mobin17@gmail.com>
2024-02-14 22:24:25 +0530

    ✅added db connection and api, async handlers

---
+++ backend/src/db/connections.db.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import { envVars } from "../../config/serverConfig";
+import { DB_NAME } from "../utils/constants";
+
+const connectDB = async (
+  connectionString = `${envVars.MONGODB_URI}/${DB_NAME}` ||
+    `mongodb://127.0.0.1:27017/${DB_NAME}`
+) => {
+  try {
+    const connectionInstance = await mongoose.connect(connectionString);
+
+    console.log(
+      `\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`
+    );
+  } catch (error) {
+    console.error("MONGODB connection error", error);
+    throw error;
+  }
+};
+export default connectDB;
