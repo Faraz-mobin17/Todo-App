3f071f47cb7ecdadd04f2f8dd28ca7e12214b264
Faraz-mobin17 <faraz.mobin17@gmail.com>
2024-02-14 20:23:07 +0530

    added prettier rc file and app.js and constants.js file

---
+++ backend/src/app.js
@@ -0,0 +1,15 @@
+import express from "express";
+import morgan from "morgan";
+
+const app = express();
+// middlewares
+app.use(morgan("dev")); // middleware for logging in terminal
+app.use(express.json()); // convert form data to json
+app.use(express.urlencoded({ extended: true, limit: LIMIT })); // send data from form and url
+app.use(express.static("public")); // use public folder for static assets
+
+app.all("*", (req, res, next) => {
+  return res.status(404).json(new ApiError(404, [], `${req.originalUrl}`));
+});
+
+export { app };
