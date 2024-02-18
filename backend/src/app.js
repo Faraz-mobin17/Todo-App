04a6545e068580f1e2e150f8dbc76dccedb72f77
Faraz-mobin17 <faraz.mobin17@gmail.com>
2024-02-18 23:32:54 +0530

    ✅ added/imported envars

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,11 @@
 import express from "express";
 import morgan from "morgan";
-
+import { envVars } from "../config/serverConfig";
 const app = express();
 // middlewares
 app.use(morgan("dev")); // middleware for logging in terminal
 app.use(express.json()); // convert form data to json
-app.use(express.urlencoded({ extended: true, limit: LIMIT })); // send data from form and url
+app.use(express.urlencoded({ extended: true, limit: envVars.LIMIT })); // send data from form and url
 app.use(express.static("public")); // use public folder for static assets
 
 app.all("*", (req, res, next) => {
