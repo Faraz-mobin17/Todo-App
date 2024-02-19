a568b7a77f32dfc39b0c90e984c08f5781c8ee61
Faraz-mobin17 <faraz.mobin17@gmail.com>
2024-02-19 22:19:38 +0530

    ✅ added pre, isPassCorr, generate tokens methods

--- backend/src/model/user.model.js
+++ backend/src/model/user.model.js
@@ -1,6 +1,7 @@
 import mongoose, { Schema } from "mongoose";
-import jsonwebtoken from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
+import { envVars } from "../../config/serverConfig";
 const userSchema = new Schema(
   {
     username: {
@@ -38,5 +39,51 @@ const userSchema = new Schema(
   },
   { timestamps: true }
 );
+/*
+ * save hone se pehele agr password modified nhi hai to next middleware ko call krdo
+ * else hash krdo password ko and database me store krdo phir
+ */
+userSchema.pre("save", async function (next) {
+  if (!this.isModified(this.password)) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+/*
+ * check kro ki current password and jo password hamne bheja hai
+ * wo same hai ki nhi
+ */
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullname: this.fullname,
+    },
+    envVars.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: envVars.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullname: this.fullname,
+    },
+    envVars.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: envVars.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
 
 export const User = mongoose.model("User", userSchema);
