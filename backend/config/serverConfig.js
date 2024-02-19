7d37d39f05e227cc55e286e1486abf4865eeb318
Faraz-mobin17 <faraz.mobin17@gmail.com>
2024-02-19 22:19:01 +0530

    ✅ added tokens expiry and secret

--- backend/config/serverConfig.js
+++ backend/config/serverConfig.js
@@ -15,6 +15,10 @@ const envVars = {
   MONGODB_URI: process.env.MONGODB_URI || "mongodb://localhost:27017",
   LIMIT: process.env.LIMIT || "16kb",
   CROSS_ORIGIN: "*",
+  ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET,
+  ACCESS_TOKEN_EXPIRY: process.env.ACCESS_TOKEN_EXPIRY,
+  REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET,
+  REFRESH_TOKEN_EXPIRY: process.env.REFRESH_TOKEN_EXPIRY,
 };
 
 export { envVars };
