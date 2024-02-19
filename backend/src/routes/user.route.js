1597f6ebb59b5eefc2413ea9c9a441ac1e47434a
Faraz-mobin17 <faraz.mobin17@gmail.com>
2024-02-19 23:06:28 +0530

    ✅ CRUD route added

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,5 +1,17 @@
 import { Router } from "express";
-
+import {
+  createTodo,
+  deleteTodoById,
+  getAllTodos,
+  updateTodoById,
+} from "../controllers/todo.controller.js";
 const router = Router();
+
+router.route("/").get(getAllTodos);
+router
+  .route("/:id")
+  .post(createTodo)
+  .patch(updateTodoById)
+  .delete(deleteTodoById);
 
 export default router;
