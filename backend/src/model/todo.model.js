21858d54fb7134427b1acfa4129f3f434b123aab
Faraz-mobin17 <faraz.mobin17@gmail.com>
2024-02-19 22:39:40 +0530

    ✅ updated todo model code

--- backend/src/model/todo.model.js
+++ backend/src/model/todo.model.js
@@ -1,31 +1,60 @@
-import mongoose, { Schema } from "mongoose";
+// import mongoose, { Schema } from "mongoose";
+
+// const todoSchema = new Schema(
+//   {
+//     todo: {
+//       type: String,
+//       required: [true, "Todo required you can't create empty todo"],
+//     },
+//     checked: {
+//       type: Boolean,
+//       default: false,
+//     },
+//     username: {
+//       type: Schema.Types.ObjectId,
+//       ref: "User",
+//     },
+//     email: {
+//       type: Schema.Types.ObjectId,
+//       ref: "User",
+//     },
+//     fullname: {
+//       type: Schema.Types.ObjectId,
+//       ref: "User",
+//     },
+//     avatar: {
+//       type: Schema.Types.ObjectId,
+//       ref: "User",
+//     },
+//   },
+//   { timestamps: true }
+// );
+
+// export const Todo = mongoose.model("Todo", todoSchema);
+
+import mongoose, { Schema, Types } from "mongoose";
 
 const todoSchema = new Schema(
   {
     todo: {
       type: String,
       required: [true, "Todo required you can't create empty todo"],
+      // Add validation if needed
+      validate: {
+        validator: (v) => v.length >= 3,
+        message: "Todo must be atleast 3 characters long",
+      },
     },
     checked: {
       type: Boolean,
       default: false,
     },
-    username: {
-      type: Schema.Types.ObjectId,
-      ref: "User",
-    },
-    email: {
-      type: Schema.Types.ObjectId,
-      ref: "User",
-    },
-    fullname: {
-      type: Schema.Types.ObjectId,
-      ref: "User",
-    },
-    avatar: {
-      type: Schema.Types.ObjectId,
-      ref: "User",
-    },
+    // Remove these fields and use the `User` model's `_id` instead
+    // username: { type: Types.ObjectId, ref: "User" },
+    // email: { type: Types.ObjectId, ref: "User" },
+    // fullname: { type: Types.ObjectId, ref: "User" },
+    // avatar: { type: Types.ObjectId, ref: "User" },
+    user: { type: Types.ObjectId, ref: "User" }, // Use this single field instead
   },
   { timestamps: true }
 );
