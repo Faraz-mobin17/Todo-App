6b7d6903d795ab6ea1e9e143767ba10d0efdbb48
Faraz-mobin17 <faraz.mobin17@gmail.com>
2024-02-19 18:51:22 +0530

    ✅ added cookie-parser, cors routes and limits.

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,13 +1,29 @@
 import express from "express";
 import morgan from "morgan";
 import { envVars } from "../config/serverConfig";
+import cookieParser from "cookie-parser";
 const app = express();
+// cors
+app.use(
+  cors({
+    origin: envVars.CROSS_ORIGIN,
+    credentials: true,
+  })
+);
 // middlewares
 app.use(morgan("dev")); // middleware for logging in terminal
-app.use(express.json()); // convert form data to json
+app.use(express.json({ limit: envVars.LIMIT })); // convert form data to json
 app.use(express.urlencoded({ extended: true, limit: envVars.LIMIT })); // send data from form and url
 app.use(express.static("public")); // use public folder for static assets
+app.use(cookieParser());
 
+// routes imports
+import userRouter from "./routes/user.route.js";
+// routes declaration
+
+app.use("/api/v1/users", userRouter);
+
+// page not found error handler
 app.all("*", (req, res, next) => {
   return res.status(404).json(new ApiError(404, [], `${req.originalUrl}`));
 });
